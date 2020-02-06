ac5e04f5f391f7e4233c9dfb24464e160a7871c2
Marcus Young <monkeyyoung3@gmail.com>
2020-02-06 00:09:49 -0800

    added ability to get input value when corresponding save button is pressed

--- script.js
+++ script.js
@@ -1,50 +1,52 @@
-const $date = $(".date");
-const $inputsForm = $(".inputs");
-const hours = ['9 AM','10 AM','11 AM','12 AM','1 PM','2 PM','3 PM','4 PM','5 PM',];
-const currentH = moment().format("H");
-const $saveButton = $(".saveButton");
+$(document).ready(function(){
+    const $date = $(".date");
+    const $inputsForm = $(".inputs");
+    const hours = ['9 AM','10 AM','11 AM','12 AM','1 PM','2 PM','3 PM','4 PM','5 PM',];
+    const currentH = moment().format("H");
 
 // See https://devhints.io/moment for moment.js cheatsheet
-$date.text(moment().format('llll'));
+    $date.text(moment().format('llll'));
 
-for(let i in hours){
-    const iTime = parseInt(i) + 9;
-    const $inputGroupDiv = $("<div>");
-    const $input = $("<input>");
-    const $hourLabel = $("<span>");
-    const $hourDiv = $("<div>");
-    const $buttonDiv = $("<div>");
-    const $saveBtn = $("<button>");
-    const $saveIcon = $("<i>");
+    for(let i in hours){
+        const iTime = parseInt(i) + 9;
+        const $inputGroupDiv = $("<div>");
+        const $input = $("<input>");
+        const $hourDiv = $("<div>");
+        const $buttonDiv = $("<div>");
+        const $saveBtn = $("<button>");
+        const $saveIcon = $("<i>");
 
-    $inputGroupDiv.addClass("input-group mb-3");
-    $input.attr("type", "text");
-    $input.addClass("form-control");
-    $hourDiv.addClass("input-group-prepend");
-    $hourLabel.text(hours[i]);
-    $hourLabel.addClass("input-group-text");
-    $buttonDiv.addClass("input-group-append");
-    $saveBtn.addClass("btn btn-primary saveButton");
-    $saveBtn.attr("type", "submit");
-    $saveIcon.addClass("material-icons");
-    $saveIcon.text("save");
-    
-    if(iTime > currentH){
-        $input.addClass("future");
-    } else if(iTime < currentH){
-        $input.addClass("past");
-    } else{
-        $input.addClass("current");
+        $inputGroupDiv.addClass("input-group mb-3");
+        $input.addClass("form-control task").attr("type", "text");
+        $hourDiv.addClass("input-group-prepend input-group-text").text(hours[i]);
+        $buttonDiv.addClass("input-group-append");
+        $saveBtn.addClass("btn btn-primary saveButton").attr("data-bind", "task")
+        // $saveBtn.attr("type", "submit");
+        $saveIcon.addClass("material-icons").text("save");
+        
+        if(iTime > currentH){
+            $input.addClass("future");
+        } else if(iTime < currentH){
+            $input.addClass("past");
+        } else{
+            $input.addClass("current");
+        }
+
+        $saveBtn.append($saveIcon);
+        $buttonDiv.append($saveBtn);
+        $inputGroupDiv.append($hourDiv, $input, $buttonDiv);
+        $inputsForm.append($inputGroupDiv);
     }
 
-    $hourDiv.append($hourLabel);
-    $saveBtn.append($saveIcon);
-    $buttonDiv.append($saveBtn);
-    $inputGroupDiv.append($hourDiv, $input, $buttonDiv);
-    $inputsForm.append($inputGroupDiv);
-}
-
-$saveButton.on('click', function(e){
-    e.preventDefault();
-    console.log('hi');
+    $(document).on('click', ".saveButton", function(e){
+        e.preventDefault();
+        console.log($(this).parents('.input-group-append').siblings('input').val());
+        // var inputt = $('.' + $(this).data('bind'));
+        // console.log(inputt);
+    })
 })
+//   $(this).parents('.input-group').children('input').val(amount - 1);
+// $(".increase").click(function() {
+//     var input = $('#' + $(this).data('bind'));
+//     input.val(input.val++);
+//   });
